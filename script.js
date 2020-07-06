fa70d8e8dd2d495764b540d695df95938e0b6cef
dasha <dashka.didyk@gmail.com>
2020-07-06 20:00:30 +0300

    part1

---
+++ script.js
@@ -0,0 +1,46 @@
+// let product = "lime";
+
+// function cook() {
+//     console.log("Cook", product);
+// }
+
+// function random() {
+//     console.log(Math.floor(Math.random() * 10));
+// }
+
+// cook();
+// product = "apple";
+// cook();
+// product = "tangerine";
+// cook();
+// random();
+// random();
+
+// function random() {
+//     console.log(Math.floor(Math.random() * 10));
+// }
+
+// const random = function() {
+//     console.log(Math.floor(Math.random() * 10));
+// }
+// random();
+// random();
+
+
+
+function cook() {
+    let product = "lime";
+    console.log("Cook", product);
+}
+
+function random() {
+    console.log(Math.floor(Math.random() * 10));
+}
+
+cook();
+product = "apple";
+cook();
+product = "tangerine";
+cook();
+random();
+random();
