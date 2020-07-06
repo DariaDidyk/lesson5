3a33f8e508b194ccc46dbc07578335e8fb23cdb4
dasha <dashka.didyk@gmail.com>
2020-07-06 21:14:34 +0300

    done

--- script.js
+++ script.js
@@ -61,16 +61,80 @@
 // cook();
 // cook();
 
-const product = "tangerine";
+// const product = "tangerine";
 
-function cook(product) {
-    /*
-    var product = 'lime';
-    */
-    console.log("Cook", product);
-}
-const random = function () {
-    console.log(Math.floor(Math.random() * 10));
-}
-cook("lime");
-console.log(product);
+// function cook(product) {
+//     /*
+//     var product = 'lime';
+//     */
+//     console.log("Cook", product);
+// }
+// const random = function () {
+//     console.log(Math.floor(Math.random() * 10));
+// }
+// cook("lime");
+// console.log(product);
+
+// const product = "tangerine";
+
+// function cook(product = 'lime') {
+//     console.log("Cook", product);
+// }
+// const random = function () {
+//     console.log(Math.floor(Math.random() * 10));
+// }
+// cook();
+    // {
+    //     /*
+    //     var product = product === underfine? : 'lime';
+    //     */
+    //     console.log("Cook", product);
+    // }
+
+
+//     const random = function (min,max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// };
+
+//     function sum(a = 0, b = 0){
+//     return a + b + random(a,b);
+// }
+//     console.log(random(0,10));
+//     console.log(sum(2,10));
+
+// function pow (a = 0, b = 0){
+//     return a ^ b;
+// }
+//     console.log(sum(2,5));
+
+// function pow = Math.pow(n, p);
+// console.log(pow);
+// for (, n < p;  i++) {
+//     return a ^^ b;
+// }
+
+// pow();
+
+
+// const pow = function (n, p) {
+// const initialNumber = n;
+// if (p === 0) return 1;
+// for (let i = 1; i < p; i++) {
+//     n *= initialNumber;
+//     }
+//     return n;
+// };
+// console.log(pow(2,5));
+
+// const sum = (a,b) => {
+//     return a +b;
+// };
+// console.log(sum(5,2));
+
+// const sum = (a,b) => a +b;
+// console.log(sum(5,2));
+
+const sum = (a,b) => a + b;
+const pow = a => pow(sum(a, 7), 2);
+
+console.log(sum(5,2));
